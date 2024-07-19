9cd1098323bca72ab498e49750bebf10f940f5ba
Matt <39469614+KosmicShovel@users.noreply.github.com>
2024-07-19 03:01:26 -0500

    Add project files.

---
+++ ExtractorContext.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Concurrent;
+using System.IO.MemoryMappedFiles;
+using CriFsV2Lib;
+
+namespace CpkExtract;
+public class ExtractorContext(string outputDirectory) {
+    private readonly ConcurrentBag<(string, Exception)> m_errors = [];
+
+    public void Extract(string file) {
+        try {
+            var outDir = outputDirectory ?? Path.Join(Path.GetDirectoryName(file), $"{Path.GetFileName(file)}_unpacked");
+
+            Directory.CreateDirectory(outDir);
+
+            using var mmf = MemoryMappedFile.CreateFromFile(file);
+            using var accessor = mmf.CreateViewAccessor();
+            var buffer = new byte[accessor.Capacity];
+            accessor.ReadArray(0, buffer, 0, buffer.Length);
+
+            using var memoryStream = new MemoryStream(buffer);
+            using var reader = CriFsLib.Instance.CreateCpkReader(memoryStream, false);
+
+            foreach (var cpkFile in reader.GetFiles()) {
+                using var rented = reader.ExtractFile(cpkFile);
+                var trimIndex = rented.Count - 1;
+                while (trimIndex > 0 && rented.Span[trimIndex] == 0x00)
+                    trimIndex--;
+
+                File.WriteAllBytes(Path.Join(outDir, cpkFile.FileName), rented.Span[..(trimIndex + 1)].ToArray());
+            }
+        } catch (Exception e) {
+            m_errors.Add((file, e));
+        }
+    }
+
+    public void LogErrors() {
+        foreach (var failedFile in m_errors) {
+            Console.WriteLine($"File {Path.GetFileName(failedFile.Item1)} failed: {failedFile.Item2.Message}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Concurrent;
+using CriFsV2Lib.Definitions.Utilities;
+
+namespace CpkExtract;
+
+internal class Program {
+    internal static void Main(string[] args) {
+        if (args.Length == 0 || args.Contains("--help") || args.Contains("-h")) {
+            Console.WriteLine("Usage: CpkExtract.exe <file-path> [output-directory]");
+            Console.WriteLine("  <file-path>          The path to the directory or file to search for .cpk files.");
+            Console.WriteLine("  [output-directory]   Optional. The directory where extracted files will be stored. Defaults to the same directory as the .cpk file.");
+            return;
+        }
+
+        var path = args[0];
+        var outputDir = args.Length > 1 ? args[1] : null;
+
+        // If path is either directory or file path
+        if (!(Directory.Exists(path) || File.Exists(path))) {
+            Console.WriteLine($"Error: The specified path '{path}' does not exist.");
+            Console.WriteLine("Usage: CpkExtract.exe <file-path> [output-directory]");
+            Console.WriteLine("  <file-path>          The path to the directory or file to search for .cpk files.");
+            Console.WriteLine("  [output-directory]   Optional. The directory where extracted files will be stored. Defaults to the same directory as the .cpk file.");
+            return;
+        }
+
+        var extractor = new ExtractorContext(outputDir);
+
+        var directoryMode = Directory.Exists(path);
+
+        if (directoryMode) {
+            var files = Directory.EnumerateFiles(Path.GetDirectoryName(path)!, "*.cpk", SearchOption.AllDirectories).Where(a => !File.GetAttributes(a).HasFlag(FileAttributes.Directory));
+            var partitioner = Partitioner.Create(files, EnumerablePartitionerOptions.NoBuffering);
+
+            Parallel.ForEach(partitioner, extractor.Extract);
+        } else {
+            extractor.Extract(path);
+        }
+        
+        extractor.LogErrors();
+
+        ArrayRental.Reset();
+    }
+}
